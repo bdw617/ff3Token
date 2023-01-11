3a03c1f76d3bf769a3285bc6fc1a24e387ca82ff
bdw666 <bdw666@gmail.com>
2023-01-11 18:14:03 -0500

    updated to use https://github.com/ubiqsecurity/ubiq-fpe-go
    not yet ready, ubiq-fpe-go needs to support a radix of at least 52 characters. PR is open against this.

--- ff3Token.go
+++ ff3Token.go
@@ -3,19 +3,22 @@ package ff3Token
 import (
 	"errors"
 
-	"github.com/capitalone/fpe/ff3"
+	fpe "github.com/ubiqsecurity/ubiq-fpe-go"
 )
 
 // this is a wrapper for the ff3 Cipher
 type Cipher struct {
-	ff3Cipher ff3.Cipher
+	cipher *fpe.FF3_1
+	tweak  []byte
 }
 
-// NewCipher initializes a new FF3 Token Cipher for encryption or decryption use key and tweak parameters.
+const radixLength = 52
+
+// NewCipher initializes a new FF3-1 Token Cipher for encryption or decryption use key and tweak parameters.
 // Radix is not exposed, since for this algorithm it must be 52 [a-zA-Z]
 func NewCipher(key []byte, tweak []byte) (Cipher, error) {
-	cipher, err := ff3.NewCipher(52, key, tweak)
-	return Cipher{ff3Cipher: cipher}, err
+	cipher, err := fpe.NewFF3_1(key, tweak, radixLength)
+	return Cipher{cipher: cipher, tweak: tweak}, err
 }
 
 // Encrypt is a wrapper around ff3.Encrypt, input must be Numeric
@@ -25,7 +28,7 @@ func (c Cipher) Encrypt(X string) (string, error) {
 		return "", errors.New("invalid input sent to Encrypt (must be numeric)")
 	}
 
-	result, err := c.ff3Cipher.Encrypt(X)
+	result, err := c.cipher.Encrypt(X, c.tweak)
 	if err != nil {
 		return "", err
 	}
@@ -40,7 +43,7 @@ func (c Cipher) Decrypt(X string) (string, error) {
 	if err != nil {
 		return newX, err
 	}
-	decrypted, err := c.ff3Cipher.Decrypt(newX)
+	decrypted, err := c.cipher.Decrypt(newX, c.tweak)
 	if err != nil {
 		return "", err
 	}
